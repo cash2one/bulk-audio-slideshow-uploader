d5e615fea0f2f70f0e66a67d2665e5055ed43333
raphey <16931750+raphey@users.noreply.github.com>
2017-10-12 20:23:01 -0700

    Updated documentation

--- README.md
+++ README.md
@@ -4,10 +4,10 @@ This project enables the creation and uploading of automatically generated video
 
 ### Sub-tasks:
 
-1) Batch convert a set of images to be equal size and letterboxed to YT proportions.
-2) Create a video from a set of images and an mp3
+1) Batch convert a set of images to be equal size and letterboxed to YT proportions (done with shell script and ffmpeg)
+2) Create a video from a set of images and an mp3 (done with shell script and melt/ffmpeg)
 3) Batch create videos
-4) Upload video in a scalable way, with metadata
+4) Upload video in a scalable way, with metadata (done with someone's youtube-uploader Python script)
 5) Batch-upload a set of created videos, or batch-create/upload a set of videos
 
 _________________________________________
--- make_slideshow.sh
+++ make_slideshow.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -
 
+#----------------------------------------------------------------
+# Example of script generated:
+
 # melt -profile atsc_720p_25 \
 # initial_sample_data/set1/images/000.PNG out=50 \
 # initial_sample_data/set1/images/001.PNG out=75 -mix 25 -mixer luma \
@@ -9,13 +12,16 @@
 # -consumer avformat:initial_sample_data/set1/images/output.mp4 vcodec=libx264 an=1
 
 #----------------------------------------------------------------
-# SETTINGS
+# Settings
+
 input_dir="initial_sample_data/set1/images"
 files=`ls ${input_dir}/*.PNG`    # Change the file type to the correct type of your images
 mp3_path="initial_sample_data/set1/BB_B-2200-A_BVE82642-1.mp3"
 fade_frames=25
 output_path="initial_sample_data/set1/slideshow_video_only.mp4"
+
 #----------------------------------------------------------------
+# 
 
 n_files=`ls -1 ${input_dir}/*.PNG | wc -l` 
 song_length=`afinfo ${mp3_path} | awk '/estimated duration/ { print$3}'`
@@ -23,9 +29,10 @@ rounded_song_length=`printf "%.0f\n" ${song_length}`
 fpi_0=$((${rounded_song_length} * 25 / ${n_files} + 1))
 fpi_1=$((${fpi_0} + ${fade_frames}))
 
+#----------------------------------------------------------------
+# Generate script
 
-# Making a melt script...
-input="melt -profile atsc_720p_25 "
+input="melt -silent -profile atsc_720p_25 "
 
 i=0
 
@@ -41,9 +48,7 @@ done
 
 input+="-consumer avformat:${output_path} vcodec=libx264 an=1"
 
-script="${input}"
+#----------------------------------------------------------------
+# Run script
 
-echo ${script}
-
-# Run it
-eval "${script}"
+eval "${input}"
