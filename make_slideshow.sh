c784b8f45853a3d5d4b537f47869f07aced9834f
raphey <16931750+raphey@users.noreply.github.com>
2017-10-12 18:36:11 -0700

    Automatic image length in slideshow maker

--- make_slideshow.sh
+++ make_slideshow.sh
@@ -1,40 +1,45 @@
 #!/bin/bash -
 
-#
-# melt -verbose \
-#-profile atsc_720p_25 \
-#000.png out=50 \
-#001.png out=75 -mix 25 -mixer luma \
-#002.png out=75 -mix 25 -mixer luma \
-#003.png out=75 -mix 25 -mixer luma \
-#004.png out=75 -mix 25 -mixer luma \
-# -consumer avformat:output.mp4 vcodec=libx264 an=1
+# melt -profile atsc_720p_25 \
+# initial_sample_data/set1/images/000.PNG out=50 \
+# initial_sample_data/set1/images/001.PNG out=75 -mix 25 -mixer luma \
+# initial_sample_data/set1/images/002.PNG out=75 -mix 25 -mixer luma \
+# initial_sample_data/set1/images/003.PNG out=75 -mix 25 -mixer luma \
+# initial_sample_data/set1/images/004.PNG out=75 -mix 25 -mixer luma \
+# -consumer avformat:initial_sample_data/set1/images/output.mp4 vcodec=libx264 an=1
 
 #----------------------------------------------------------------
 # SETTINGS
-input_dir="initial_sample_data/set1/images"  # Replace this by a path to your folder /path/to/your/folder
+input_dir="initial_sample_data/set1/images"
 files=`ls ${input_dir}/*.PNG`    # Change the file type to the correct type of your images
-# n_files=`ls ${input_dir}/*.PNG -1 | wc -l`                        # Replace this by a number of images
-output_file="video.mp4"           # Name of output video
-crossfade=0.9                     # Crossfade duration between two images
+mp3_path="initial_sample_data/set1/BB_B-2200-A_BVE82642-1.mp3"
+fade_frames=25
+output_path="initial_sample_data/set1/slideshow_video_only.mp4"
 #----------------------------------------------------------------
 
-# Making an ffmpeg script...
-input="melt -verbose -profile atsc_720p_25"
+n_files=`ls -1 ${input_dir}/*.PNG | wc -l` 
+song_length=`afinfo ${mp3_path} | awk '/estimated duration/ { print$3}'`
+rounded_song_length=`printf "%.0f\n" ${song_length}`
+fpi_0=$((${rounded_song_length} * 25 / ${n_files} + 1))
+fpi_1=$((${fpi_0} + ${fade_frames}))
+
+
+# Making a melt script...
+input="melt -profile atsc_720p_25 "
 
 i=0
 
 for f in ${files}; do
   if [ "${i}" -eq "0" ]
   then
-    input+="${f} out=50 "
+    input+="${f} out=${fpi_0} "
   else
-    input+="${f} out=75 -mix 25 -mixer luma "
+    input+="${f} out=${fpi_1} -mix ${fade_frames} -mixer luma "
   fi
   i=$((i+1))
 done
 
-input+="-consumer avformat:output.mp4 vcodec=libx264 an=1"
+input+="-consumer avformat:${output_path} vcodec=libx264 an=1"
 
 script="${input}"
 
